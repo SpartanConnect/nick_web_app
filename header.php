5adf82b0ee746b978ae0d1f953efd6623db8008b
Nick Lysek <nrlysek@gmail.com>
2017-06-14 21:57:09 -0700

    Complete rewrite. I'm starting from scratch, using PHP and comments and better HTML, CSS, etc. No JS yet and less functionality then before.

---
+++ header.php
@@ -0,0 +1,44 @@
+<?php
+function headerMeta() {
+  //Initialize variables for the meta and og meta.
+  $title="Spartan Connect";
+  $description="A 21st Century Announcement App For La Ca&ntilde;ada High School";
+  //echo back the meta tags and other title stuff that's the same on all pages.
+  echo '
+    <meta charset="UTF-16">
+
+    <!-- Deal with titles, both in native HTML and og -->
+    <title>Spartan Connect</title>
+    <meta property="og:title" content='.$title.'>
+
+    <!-- Deal with native and OG description stuff -->
+    <meta property="og:description content='.$description.'>
+    <meta name="description" content='.$description.'>
+
+    <!-- Deal with stylesheet linking -->
+    <link rel="stylesheet" href="Stylesheets/header.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,500" rel="stylesheet">
+
+    <!-- Deal with other og metadata -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="http://connect.lchsspartans.net/">
+    <meta property="og:image" content="Images/SpartanConnectLogo2.png">
+
+    <!-- Deal with keywords and stuff for native html (hopefully for SEO) -->
+    <meta name="keywords" content="La Cañada High School, LCHS, Spartan Connect">
+    ';
+}
+
+function headerHTML(){
+  echo '
+    <div id="nav">
+      <div id="nav_links">
+        <!-- Add the nav links -->
+        <a href="index.php">Home</a>'.
+        /* I will have to add some if\'s here in order to handle sign-in specific nav links */
+        '</div>
+    </div>
+  ';
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include 'header.php';
+?>
+
+<html>
+  <head>
+    <?php headerMeta(); ?>
+  </head>
+  <body>
+    <?php headerHTML(); ?>
+  </body>
+</html>
